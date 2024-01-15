208767c00edfd20938cfbea4c388955c1154aa41
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2024-01-15 12:33:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import date
+import re
 import pdfplumber
 import pandas as pd
 from PyPDF2 import PdfReader
@@ -11,12 +12,15 @@ numero_de_paginas = len(reader.pages)
 # listas para armazenar os dados
 data = []
 instalacoes = []
+valores = []
+index = -1
 
 # transformar pdf em txt
 with open("fatura.txt", "w", encoding='UTF-8') as output_file:
     for page_num in range(1, numero_de_paginas - 2):
         page = reader.pages[page_num]
         text = page.extract_text()
+        #print(text)
         output_file.write(text)
 
 # extrair informações do arquivo txt
@@ -30,9 +34,18 @@ with open('fatura.txt', 'r', encoding='UTF-8') as f:
             info_instalacao = line.split()[1]
             instalacoes.append(info_instalacao)
 
+        if "Valor:" in line:
+            padrao = re.compile(r'Recolhimento:(.*?)Valor: (\d+\.\d+)', re.DOTALL)
+            resultados = padrao.findall(line)
+
+        # Imprima os resultados
+            for resultado in resultados:
+                valores.append(resultado[1])
+                
+
 # função para extrair informações do arquivo PDF
 with pdfplumber.open("fatura.pdf") as pdf:
-    for page_num in range(1, numero_de_paginas - 2):
+    for page_num in range(1, 5 - 2):
         first_page = pdf.pages[page_num]
         text = first_page.extract_text()
         
@@ -42,13 +55,15 @@ with pdfplumber.open("fatura.pdf") as pdf:
                 leitura_anterior = line.split()[4]
                 leitura_atual = line.split()[5]
                 medido = line.split()[6]
+                index = index + 1
 
                 # adiciona as informações à lista de dados
                 data.append({
-                    'Instalação': instalacoes[-1],
+                    'Instalação': int(instalacoes[index]),
                     'Leit.Anterior': leitura_anterior,
                     'Leit.Atual': leitura_atual,
-                    'Medido': medido
+                    'Medido': medido,
+                    'Valor': valores[index]
                 })
 
 df = pd.DataFrame(data)
